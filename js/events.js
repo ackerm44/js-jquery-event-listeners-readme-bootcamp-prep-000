d2ad1ad30b593802339bead98ab853a2ff0a0a53
Amanda Ackerman <ackerm44@gmail.com>
2017-10-05 16:37:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,6 +4,16 @@ function getIt() {
   })
 }
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty'); 
+  })
+}
+
+function pressIt() {
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
